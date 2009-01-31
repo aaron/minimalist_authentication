0b124fd48ee1e0656f9142c5cbf05ca662e8b2bb
Aaron Baldwin <baldwina@gmail.com>
2009-01-30 23:50:33 -0700

    add tests for login should fail scenarios

--- test/minimalist_authentication_test.rb
+++ test/minimalist_authentication_test.rb
@@ -20,6 +20,26 @@ class MinimalistAuthenticationTest < ActiveSupport::TestCase
     assert_equal(user, User.authenticate(user.email, 'password'))
   end
   
+  test "should fail to authenticate when email is blank" do
+    user = Factory(:user)
+    assert_nil(User.authenticate('', 'password'))
+  end
+  
+  test "should fail to authenticate when password is blank" do
+    user = Factory(:user)
+    assert_nil(User.authenticate(user.email, ''))
+  end  
+  
+  test "should fail to authenticate when user is not active" do
+    user = Factory(:user, :active => false)
+    assert_nil(User.authenticate(user.email, 'password'))
+  end
+  
+  test "should fail to authenticate for incorrect password" do
+    user = Factory(:user)
+    assert_nil(User.authenticate(user.email, 'incorrect_password'))
+  end
+  
   test "should create salt and encrypted_password for new user" do
     user = User.new(:email => 'test@testing.com', :password => 'testing')
     assert(user.save)
